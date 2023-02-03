d36b2ef9d8e66c8f3ac9f4bf988e942fe3ad21a4
David Grammatico <david.lugdunum@gmail.com>
2023-02-03 16:56:33 +0100

    full intallation

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+}
+module.exports = createJestConfig(customJestConfig)

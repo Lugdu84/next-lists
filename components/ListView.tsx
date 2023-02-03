bf365ccdc2d7818dcbab658463d2bcb8c4163d1e
David Grammatico <david.lugdunum@gmail.com>
2023-02-03 22:50:55 +0100

    update listView design

--- components/ListView.tsx
+++ components/ListView.tsx
@@ -7,9 +7,11 @@ function ListView() {
   const lists = useLists()
   return (
     <div className="flex flex-col space-y-3">
-      {lists.map((list) => (
-        <ListItem key={list.id} list={list} />
-      ))}
+      {lists.length > 0 ? (
+        lists.map((list) => <ListItem key={list.id} list={list} />)
+      ) : (
+        <div>Nous n&apos;avez pas encore de liste...</div>
+      )}
       <ListForm />
     </div>
   )
